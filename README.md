c5511600bebeb852227ce439e477e7dddc11a961
Wei Su <wei@funxociety.com>
2022-11-27 18:08:32 +0800

    fix: update style and README

--- README.md
+++ README.md
@@ -1,34 +1,9 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# NeoLand
 
-## Getting Started
+"NeoLand is a collection of plugins, themes and other resources for NeoVim."
 
-First, run the development server:
+👆 GitHub Copilot said so, I guess we've got lots do.
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+Build upon [Awesome Neovim](https://github.com/rockerBOO/awesome-neovim#wishlist)
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+`Let the wonderland thrive.`
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 // @ts-nocheck
+import { createClient } from "@supabase/supabase-js";
 import React from "react";
-import { Octokit } from "octokit";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 export type PluginCardProps = {
@@ -11,7 +9,7 @@ export type PluginCardProps = {
   description: string;
   author: string;
   link: string;
-  lastUpdated: string;
+  last_updated: string;
   stars: number;
   avatar: string;
 };
@@ -21,33 +19,42 @@ const PluginCard = ({
   description,
   author,
   link,
-  lastUpdated,
+  last_updated,
   stars,
   avatar,
 }: PluginCardProps) => {
   return (
-    <div className="card bg-base-100 shadow-xl w-[800px]  mx-auto">
-      <div className="card-body flex flex-row justify-between">
-        <div className="flex flex-col gap-y-4">
-          <h2 className="card-title text-3xl">{name}</h2>
-          <p>{description}</p>
+    <div className="card bg-base-100 shadow-xl w-full max-w-[800px] mx-auto">
+      <div className="card-body flex justify-between">
+        <div className="flex flex-col w-full gap-y-4">
+          <div className="flex justify-between items-center w-full gap-x-4">
+            <h2 className="card-title text-xl md:text-3xl max-w-1/2 text-ellipsis overflow-hidden block">
+              {name}
+            </h2>
+            <a
+              className="btn gap-2"
+              href={link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                width="24"
+                height="24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+                ></path>
+              </svg>
+              <p className="hidden md:block">GitHub</p>
+            </a>
+          </div>
+          <p className="text-sm md:text-base">{description}</p>
         </div>
-        <a className="btn gap-2" href={link} target="_blank" rel="noreferrer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            width="24"
-            height="24"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-            ></path>
-          </svg>
-          GitHub
-        </a>
       </div>
-      <div className="stats shadow">
+      <div className="stats stats-vertical lg:stats-horizontal  shadow">
         <div className="stat">
           <div className="stat-title">Total Stars</div>
           <div className="stat-value text-secondary text-xl">{stars}</div>
@@ -56,8 +63,8 @@ const PluginCard = ({
           <div className="stat-title">Last Updated </div>
           <div className="flex">
             <div className="stat-value  text-xl">
-              {lastUpdated
-                ? formatDistanceToNow(new Date(lastUpdated), {
+              {last_updated
+                ? formatDistanceToNow(new Date(last_updated), {
                     addSuffix: true,
                   })
                 : "-"}
@@ -87,64 +94,58 @@ const PluginCard = ({
 };
 export default function Home({ data = [] }: any) {
   return (
-    <div className="bg-neutral min-h-screen">
+    <div className="bg-neutral min-h-screen pb-16">
       <Head>
         <title>NeoLand</title>
         <meta name="description" content="All about NeoVim" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="py-16 text-center">
-        <h1 className="text-5xl font-black font-hubot">NeoLand</h1>
+      <header className="px-8 py-16 flex flex-col items-center relative gap-y-4">
+        <h1 className="text-5xl font-black font-hubot flex-1 text-center">
+          NeoLand
+        </h1>
+        <p>
+          " NeoLand is a collection of plugins, themes and other resources for
+          NeoVim. "
+        </p>
+        <p>👆 GitHub Copilot said so, I guess we've got lots do.</p>
+        <a
+          className="btn btn-outline btn-active gap-2 absolute right-16"
+          href="https://github.com/akasuv/neoland"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            width="24"
+            height="24"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+            ></path>
+          </svg>
+          <p className="hidden md:block">GitHub</p>
+        </a>
       </header>
-      <main className="flex flex-col gap-y-4">
+      <main className="flex flex-col gap-y-4 px-8">
         {data.map((item: any) => (
           <PluginCard {...(item as PluginCardProps)} />
         ))}
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div></div>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const octokit = new Octokit({
-    auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
-  });
-  async function getRepoStats(author: string, repoName: string) {
-    return await octokit.request("GET /repos/{owner}/{repo}", {
-      owner: author,
-      repo: repoName,
-    });
-  }
-  let data = await fetch("https://neoland.vercel.app/api/hello").then((res) =>
-    res.json()
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
 
-  const additionalData = data.map(
-    async (item: any) => await getRepoStats(item.author, item.name)
-  );
-
-  const res = await Promise.allSettled(additionalData);
-
-  res.forEach((item, idx) => {
-    data[idx] = {
-      ...data[idx],
-      stars: item.value?.data?.stargazers_count,
-      lastUpdated: item.value?.data?.updated_at,
-      avatar: item.value?.data?.owner.avatar_url,
-    };
-  });
+  const { data, error } = await supabase.from("Plugins").select();
 
   return { props: { data } };
 }
