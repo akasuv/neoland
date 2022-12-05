2c32f855910e200090ec291cacad4495d9953c21
Wei <wei@funxociety.com>
2022-12-05 21:17:38 +0300

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <title>NeoLand</title>
-      <meta name="description" content="All about NeoVim" />
+      <meta name="description" content="All about Neovim" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
