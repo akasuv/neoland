7cee24a3c56b9fa1829698b3cc8d83edc1443fdd
Wei Su <wei@funxociety.com>
2022-12-07 23:22:40 +0800

    fix: homepage responsive ui

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title>NeoLand</title>
       <meta name="description" content="All about Neovim" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -9,7 +9,7 @@ import React from "react";
 import Link from "next/link";
 
 const Card = ({ title, icon }: { title: string; icon: IconDefinition }) => (
-  <div className="h-30 w-full flex flex-col pt-2  border shadow-sm rounded-xl  bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 border-gray-700 shadow-slate-700/[.7]">
+  <div className="h-30 md:h-30 w-full flex flex-col pt-2  border shadow-sm rounded-xl  bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 border-gray-700 shadow-slate-700/[.7]">
     <div className="flex flex-auto flex-col justify-center items-center p-4 md:p-3 gap-y-3">
       <FontAwesomeIcon icon={icon} size="2x" />
       <h3 className="text-lg font-bold">{title}</h3>
@@ -23,12 +23,9 @@ const sections = [
 ];
 
 const Hero = () => (
-  <div className="flex gap-x-4 w-full justify-center">
+  <div className="flex gap-4 w-full justify-center flex-col md:flex-row items-center">
     {sections.map((section) => (
-      <Link
-        href={section.href}
-        className="grow max-w-[280px] min-w-[100px] w-[100px] shrink-0"
-      >
+      <Link href={section.href} className="grow max-w-[280px] w-full ">
         <Card title={section.title} icon={section.icon} />
       </Link>
     ))}
