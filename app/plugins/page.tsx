d23d5eb9af0dbce1af1f1f5390ee9b8ac8837e34
Wei Su <wei@funxociety.com>
2022-12-08 00:01:38 +0800

    fix: update plugins page style

--- app/plugins/page.tsx
+++ app/plugins/page.tsx
@@ -28,26 +28,26 @@ const Plugins = async () => {
       <div className="flex items-center w-full justify-center my-8 py-20 bg-gradient-to-b from-gray-900 to-gray-800 bg-gradient-to-r">
         <h2 className="text-5xl font-black text-white font-hubot">Plugins</h2>
       </div>
-      <div className="flex gap-x-4 w-full px-8">
-        <div className="flex flex-col w-80 grow w-60 p-4 gap-y-4">
-          <ul className="flex flex-col divide-y divide-gray-200 dark:divide-gray-700">
-            <li className="inline-flex items-center gap-x-2 py-3 text-sm font-medium text-gray-800 dark:text-white">
-              Popular
-            </li>
-            <li className="inline-flex items-center gap-x-2 py-3 text-sm font-medium text-gray-800 dark:text-white">
-              New
-            </li>
-            <li className="inline-flex items-center gap-x-2 py-3 text-sm font-medium text-gray-800 dark:text-white">
-              All
-            </li>
-          </ul>
-          <div className="flex flex-wrap gap-4">
-            {tags?.data?.slice(0, 10).map((tag) => (
+      <div className="flex gap-x-4 w-full px-8 flex-col md:flex-row">
+        <div className="flex flex-col md:w-80 grow p-4 gap-y-4 w-full mb-8">
+          {/* <ul className="flex flex-col divide-y divide-gray-200 dark:divide-gray-700"> */}
+          {/*   <li className="inline-flex items-center gap-x-2 py-3 text-sm font-medium text-gray-800 dark:text-white"> */}
+          {/*     Popular */}
+          {/*   </li> */}
+          {/*   <li className="inline-flex items-center gap-x-2 py-3 text-sm font-medium text-gray-800 dark:text-white"> */}
+          {/*     New */}
+          {/*   </li> */}
+          {/*   <li className="inline-flex items-center gap-x-2 py-3 text-sm font-medium text-gray-800 dark:text-white"> */}
+          {/*     All */}
+          {/*   </li> */}
+          {/* </ul> */}
+          <div className="flex flex-wrap gap-4 w-full">
+            {tags?.data?.map((tag) => (
               <Tag text={tag.primary} />
             ))}
           </div>
         </div>
-        <div className="px-4 flex flex-col gap-y-4 w-3/4 grow-0">
+        <div className="px-4 flex flex-col gap-y-4 sm:w-full md:w-3/4 grow-0">
           {data?.map((plugin: any) => (
             <PluginCard {...plugin} />
           ))}
