c8d3e343cf1ee5330c0f270f84df04ec23256732
Wei Su <wei@funxociety.com>
2022-12-11 02:09:32 +0800

    fix: change og image params

--- app/head.tsx
+++ app/head.tsx
@@ -7,14 +7,16 @@ export default function Head() {
 				property="og:description"
 				content="Find the best Neovim resources on NeoLand."
 			/>
-			<meta property="og:image" content="https://i.imgur.com/RFAa9nL.png" />
+			<meta property="og:image" content="https://i.imgur.com/sUMHQ5M.png" />
+			<meta property="og:image:width" content="1200" />
+			<meta property="og:image:height" content="600" />
 			<meta name="twitter:card" content="summary" />
 			<meta name="twitter:title" content="NeoLand" />
 			<meta
 				name="twitter:description"
 				content="Find the best Neovim resources on NeoLand."
 			/>
-			<meta name="twitter:image" content="https://i.imgur.com/RFAa9nL.png" />
+			<meta name="twitter:image" content="https://i.imgur.com/sUMHQ5M.png" />
 
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<link rel="icon" href="/favicon.ico" />
